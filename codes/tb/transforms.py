ce43cd8aacc3308c31acb2f6d36de9db2c1a5a57
Johanna <johanna@zijderveld.de>
2024-04-12 14:25:23 +0200

    move and delete for sure unused functions

--- codes/tb/transforms.py
+++ codes/tb/transforms.py
@@ -171,3 +171,50 @@ def kfunc2tb(kfunc, nSamples, ndim=1):
         raise NotImplementedError("n > 2 not implemented")
     ifftnArray = ifftn(kfuncOnGrid, axes=np.arange(ndim))
     return ifftn2tb(ifftnArray)
+
+
+def hk2h_0(hk, hopping_vecs, ks=None):
+    """
+    Extract hopping matrices from Bloch Hamiltonian.
+
+    Parameters:
+    -----------
+    hk : nd-array
+        Bloch Hamiltonian matrix hk[k_x, ..., k_n, i, j]
+    h_0 : dict
+        Tight-binding model of non-interacting systems.
+    h_int : dict
+        Tight-binding model for interacting Hamiltonian.
+    ks : 1D-array
+        Set of k-points. Repeated for all directions. If the system is finite, `ks=None`.
+
+    Returns:
+    --------
+    scf_model : dict
+        TIght-binding model of Hartree-Fock solution.
+    """
+    if ks is not None:
+        ndim = len(hk.shape) - 2
+        dk = np.diff(ks)[0]
+        nk = len(ks)
+        k_pts = np.tile(ks, ndim).reshape(ndim, nk)
+        k_grid = np.array(np.meshgrid(*k_pts))
+        k_grid = k_grid.reshape(k_grid.shape[0], np.prod(k_grid.shape[1:]))
+        hk = hk.reshape(np.prod(hk.shape[:ndim]), *hk.shape[-2:])
+
+        hopps = (
+            np.einsum(
+                "ij,jkl->ikl",
+                np.exp(1j * np.einsum("ij,jk->ik", hopping_vecs, k_grid)),
+                hk,
+            )
+            * (dk / (2 * np.pi)) ** ndim
+        )
+
+        h_0 = {}
+        for i, vector in enumerate(hopping_vecs):
+            h_0[tuple(vector)] = hopps[i]
+
+        return h_0
+    else:
+        return {(): hk}
