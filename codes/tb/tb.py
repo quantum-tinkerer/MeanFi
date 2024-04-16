f365ca9a2f2e2131679a0756b6bcdcb48c57891f
Johanna <johanna@zijderveld.de>
2024-04-16 15:35:52 +0200

    add tolerance option to compare_dicts

--- codes/tb/tb.py
+++ codes/tb/tb.py
@@ -20,6 +20,6 @@ def add_tb(tb1, tb2):
     return {k: tb1.get(k, 0) + tb2.get(k, 0) for k in frozenset(tb1) | frozenset(tb2)}
 
 
-def compare_dicts(dict1, dict2):
+def compare_dicts(dict1, dict2, atol=1e-10):
     for key in dict1.keys():
-        assert np.allclose(dict1[key], dict2[key])
+        assert np.allclose(dict1[key], dict2[key], atol=atol)
