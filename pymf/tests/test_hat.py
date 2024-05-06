58bf8eaeb66b7e7b9b3c6b4b03653359bb4280f7
Johanna <johanna@zijderveld.de>
2024-05-06 14:05:46 +0200

    extend mexican hat test to 0, 2 and 3d

--- pymf/tests/test_hat.py
+++ pymf/tests/test_hat.py
@@ -17,37 +17,63 @@ def total_energy(ham_tb, rho_tb):
 # %%
 U0 = 1
 filling = 2
-nk = 100
+nk = 20
 repeat_number = 10
 
 hopp = np.kron(np.array([[0, 1], [0, 0]]), np.eye(2))
-h_0 = {(0,): hopp + hopp.T.conj(), (1,): hopp, (-1,): hopp.T.conj()}
-h_int_U0 = {
+
+# 0D
+h0_0d = {(): hopp + hopp.T.conj()}
+h_int_0d = {(): U0 * np.kron(np.eye(2), np.ones((2, 2)))}
+
+# 1D
+h0_1d = {(0,): hopp + hopp.T.conj(), (1,): hopp, (-1,): hopp.T.conj()}
+h_int_1d = {
     (0,): U0 * np.kron(np.eye(2), np.ones((2, 2))),
 }
+
+# 2D
+h0_2d = {
+    (0, 0): hopp + hopp.T.conj(),
+    (0, 1): hopp,
+    (0, -1): hopp.T.conj(),
+}
+h_int_2d = {
+    (0, 0): U0 * np.kron(np.eye(2), np.ones((2, 2))),
+}
+
+# 3D
+h0_3d = {(0, 0, 0): hopp + hopp.T.conj(), (0, 0, 1): hopp, (0, 0, -1): hopp.T.conj()}
+h_int_3d = {
+    (0, 0, 0): U0 * np.kron(np.eye(2), np.ones((2, 2))),
+}
+
+h0s = [h0_0d, h0_1d, h0_2d, h0_3d]
+h_ints = [h_int_0d, h_int_1d, h_int_2d, h_int_3d]
 
 
 # %%
 @np.vectorize
-def mf_rescaled(alpha, mf0):
-    hamiltonian = add_tb(h_0, scale_tb(mf0, alpha))
+def mf_rescaled(alpha, mf0, h0):
+    hamiltonian = add_tb(h0, scale_tb(mf0, alpha))
     rho, _ = mf.construct_density_matrix(hamiltonian, filling=filling, nk=nk)
-    hamiltonian = add_tb(h_0, scale_tb(mf0, np.sign(alpha)))
+    hamiltonian = add_tb(h0, scale_tb(mf0, np.sign(alpha)))
     return total_energy(hamiltonian, rho)
 
 
 @pytest.mark.parametrize("seed", range(repeat_number))
 def test_mexican_hat(seed):
     np.random.seed(seed)
-    guess = utils.generate_guess(frozenset(h_int_U0), len(h_int_U0[(0,)]))
-    _model = Model(h_0, h_int_U0, filling=filling)
-    mf_sol_groundstate = solver(
-        _model, mf_guess=guess, nk=nk, optimizer_kwargs={"M": 0}
-    )
+    for h0, h_int in zip(h0s, h_ints):
+        guess = utils.generate_guess(frozenset(h_int), len(hopp))
+        _model = Model(h0, h_int, filling=filling)
+        mf_sol_groundstate = solver(
+            _model, mf_guess=guess, nk=nk, optimizer_kwargs={"M": 0}
+        )
 
-    alphas = np.random.uniform(0, 50, 100)
-    alphas = np.where(alphas == 1, 0, alphas)
-    assert np.all(
-        mf_rescaled(alphas, mf0=mf_sol_groundstate)
-        > mf_rescaled(np.array([1]), mf0=mf_sol_groundstate)
-    )
+        alphas = np.random.uniform(0, 50, 100)
+        alphas = np.where(alphas == 1, 0, alphas)
+        assert np.all(
+            mf_rescaled(alphas, mf0=mf_sol_groundstate, h0=h0)
+            > mf_rescaled(np.array([1]), mf0=mf_sol_groundstate, h0=h0)
+        )
