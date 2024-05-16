6420eb3e2214c4a67ee61f4c20616b1804bb2734
Kostas Vilkelis <kostasvilkelis@gmail.com>
2024-05-16 12:46:14 +0200

    clean up parametrisation

--- meanfi/params/rparams.py
+++ meanfi/params/rparams.py
@@ -1,10 +1,27 @@
 import numpy as np
 
-from meanfi.params.param_transforms import (
-    flat_to_tb,
-    tb_to_flat,
-)
 from meanfi.tb.tb import _tb_type
+
+
+def complex_to_real(z: np.ndarray) -> np.ndarray:
+    """Split and concatenate real and imaginary parts of a complex array.
+
+    Parameters
+    ----------
+    z :
+        Complex array.
+
+    Returns
+    -------
+    :
+        Real array that concatenates the real and imaginary parts of the input array.
+    """
+    return np.concatenate((np.real(z), np.imag(z)))
+
+
+def real_to_complex(z: np.ndarray) -> np.ndarray:
+    """Undo `complex_to_real`."""
+    return z[: len(z) // 2] + 1j * z[len(z) // 2 :]
 
 
 def tb_to_rparams(tb: _tb_type) -> np.ndarray:
@@ -20,7 +37,20 @@ def tb_to_rparams(tb: _tb_type) -> np.ndarray:
     :
         1D real vector that parametrises the tight-binding dictionary.
     """
-    return tb_to_flat(tb)
+    ndim = len(list(tb)[0])
+    onsite_key = tuple(np.zeros((ndim,), dtype=int))
+
+    hopping_keys = sorted([key for key in tb.keys() if key != onsite_key])
+    hopping_keys = hopping_keys[: len(hopping_keys) // 2]
+
+    onsite_upper_vals = tb[onsite_key][
+        np.triu_indices(tb[onsite_key].shape[-1], k=1)
+    ].flatten()
+    onsite_diag_vals = np.diag(tb[onsite_key]).real.flatten()
+
+    hopping_vals = [tb[key].flatten() for key in hopping_keys]
+    complex_vals = np.concatenate([onsite_upper_vals, *hopping_vals])
+    return np.concatenate([onsite_diag_vals, complex_to_real(complex_vals)])
 
 
 def rparams_to_tb(
@@ -42,4 +72,35 @@ def rparams_to_tb(
     :
         Tight-biding dictionary.
     """
-    return flat_to_tb(tb_params, ndof, tb_keys)
+    ndim = len(tb_keys[0])
+
+    hopping_shape = (len(tb_keys) - 1, ndof, ndof)
+
+    onsite_idxs = ndof + ndof * (ndof - 1) // 2
+    onsite_key = tuple(np.zeros((ndim,), dtype=int))
+
+    # reconstruct the complex values
+    onsite_diag_vals = tb_params[:ndof]
+    complex_vals = real_to_complex(tb_params[ndof:])
+    onsite_upper_vals = complex_vals[: onsite_idxs - ndof]
+    hopping_vals = complex_vals[(onsite_idxs - ndof) :]
+
+    # first build onsite matrix
+    onsite_matrix = np.zeros((ndof, ndof), dtype=complex)
+    onsite_matrix[np.triu_indices(ndof, k=1)] = onsite_upper_vals
+    onsite_matrix += onsite_matrix.conj().T
+    onsite_matrix[np.diag_indices(ndof)] = onsite_diag_vals
+
+    # then build hopping matrices
+    hopping_matrices = np.zeros(hopping_shape, dtype=complex)
+    N = len(tb_keys) // 2
+    hopping_matrices[:N] = hopping_vals.reshape(N, *hopping_shape[1:])
+    hopping_matrices[N:] = np.moveaxis(
+        np.flip(hopping_matrices[:N], axis=0), -1, -2
+    ).conj()
+
+    # combine all into a dictionary
+    hopping_keys = sorted([key for key in tb_keys if key != onsite_key])
+    tb = {key: hopping_matrices[i] for i, key in enumerate(hopping_keys)}
+    tb[onsite_key] = onsite_matrix
+    return tb
