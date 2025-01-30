37b05b2df3a2afb9edb2ce3ebee9a9be9a0e7120
kyudomaster <jasper.brookman@kpnmail.nl>
2025-01-30 15:50:56 +0100

    Tried to ignore the white-spaces (but reverted the change.)

--- meanfi/tests/test_minimization.py
+++ meanfi/tests/test_minimization.py
@@ -1,3 +1,4 @@
+# %%
 import numpy as np
 import pytest
 
@@ -6,17 +7,18 @@ from meanfi.tb.utils import generate_tb_keys, guess_tb
 
 
 repeat_number = 5
-ndim = 1
-ndof = 4
-cutoff = 2
-nk = 500
-filling = ndof / 2
-kT = 1e-3
 
 
-@pytest.mark.parametrize("seed", range(repeat_number))
-def test_minimizer_consistency(seed):
-    np.random.seed(seed)
+# %%
+def minimizer_offset(
+    cutoff: int,
+    ndim: int,
+    ndof: int,
+    filling: float,
+    nk: int,
+    kT: float,
+    f_random: float,
+):
     keys = generate_tb_keys(cutoff, ndim)
     h_0 = guess_tb(keys, ndof)
 
@@ -27,14 +29,25 @@ def test_minimizer_consistency(seed):
     h_shift = add_tb(h_0, _shift)
 
     # Generate an offset Hamiltonian.
-    f_random = np.random.uniform(-2, 2)
+
     _offset = {(0,) * ndim: f_random * np.eye(ndof)}
     h_offset = add_tb(h_shift, _offset)
 
     # Compute f_offset for the offset Hamiltonian.
     f_offset = density_matrix(h_offset, filling, nk, kT)[1]
-    print(f_level, f_random, f_offset)
+
     assert np.allclose(f_random, f_offset, kT / 2, kT / 2)
 
 
-test_minimizer_consistency(65678)
+# %%
+@pytest.mark.parametrize("seed", range(repeat_number))
+def test_minimizer_consistency(seed):
+    np.random.seed(seed)
+    ndim = np.random.randint(1, 4)
+    ndof = np.random.randint(1, 8)
+    cutoff = np.random.randint(1, 5)
+    nk = np.random.randint(10, 1000)
+    filling = np.random.uniform(0, ndof)
+    kT = np.random.uniform(0, 1e-2)
+    f_random = np.random.uniform(-3, 3)
+    minimizer_offset(cutoff, ndim, ndof, filling, nk, kT, f_random)
