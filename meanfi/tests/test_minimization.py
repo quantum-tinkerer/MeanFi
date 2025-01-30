0b1acaf7330cd39b1d233bf34b3a84a02d4fea45
kyudomaster <jasper.brookman@kpnmail.nl>
2025-01-30 14:56:40 +0100

    Pre-commit testing

--- meanfi/tests/test_minimization.py
+++ meanfi/tests/test_minimization.py
@@ -1,10 +1,7 @@
 import numpy as np
 import pytest
 
-from meanfi.mf import (
-    add_tb,
-    density_matrix
-    )
+from meanfi.mf import add_tb, density_matrix
 from meanfi.tb.utils import generate_tb_keys, guess_tb
 
 
@@ -14,7 +11,7 @@ ndof = 4
 cutoff = 2
 nk = 500
 filling = ndof / 2
-kT = 1e-5
+kT = 1e-3
 
 
 @pytest.mark.parametrize("seed", range(repeat_number))
@@ -24,17 +21,20 @@ def test_minimizer_consistency(seed):
     h_0 = guess_tb(keys, ndof)
 
     f_level = density_matrix(h_0, filling, nk, kT)[1]
-    
+
     # Shift the Hamiltonian.
-    _shift  = {(0,)*ndim: -f_level * np.eye(ndof)}
+    _shift = {(0,) * ndim: -f_level * np.eye(ndof)}
     h_shift = add_tb(h_0, _shift)
 
     # Generate an offset Hamiltonian.
     f_random = np.random.uniform(-2, 2)
-    _offset  = {(0,)*ndim: f_random * np.eye(ndof)}
+    _offset = {(0,) * ndim: f_random * np.eye(ndof)}
     h_offset = add_tb(h_shift, _offset)
 
     # Compute f_offset for the offset Hamiltonian.
     f_offset = density_matrix(h_offset, filling, nk, kT)[1]
     print(f_level, f_random, f_offset)
-    assert np.allclose(f_random, f_offset, kT/2, kT/2)
+    assert np.allclose(f_random, f_offset, kT / 2, kT / 2)
+
+
+test_minimizer_consistency(65678)
