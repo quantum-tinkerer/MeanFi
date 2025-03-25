0d8d2a98c950e0c6d5cf43125acfa82ee86fa1f7
kyudomaster <jasper.brookman@kpnmail.nl>
2025-03-25 01:03:38 +0100

    Added the symmetry solvers to `solvers.py` (Not complete yet)

--- meanfi/solvers.py
+++ meanfi/solvers.py
@@ -8,6 +8,7 @@ from meanfi.tb.tb import add_tb, _tb_type
 from meanfi.mf import density_matrix, meanfield
 from meanfi.model import Model
 from meanfi.tb.utils import fermi_energy
+import utils.tb_to_qsymm as tb_to_qsymm
 
 
 def cost_mf(mf_param: np.ndarray, model: Model, nk: int = 20) -> np.ndarray:
@@ -69,6 +70,44 @@ def cost_density(rho_params: np.ndarray, model: Model, nk: int = 20) -> np.ndarr
     return rho_params_new - rho_params
 
 
+def cost_density_symmetric(
+    rho_params: dict, model: Model, Q_basis: dict, nk: int = 20
+) -> np.ndarray:
+    """Defines the cost function for root solver.
+
+    The cost function is the difference between the computed and inputted density matrix
+    reduced to the hoppings only present in the h_int.
+
+    Parameters
+    ----------
+    rho_params :
+        1D real array that parametrises the density matrix reduced to the
+        hoppings (keys) present in h_int.
+    Model :
+        Interacting tight-binding problem definition.
+    nk :
+        Number of k-points in a grid to sample the Brillouin zone along each dimension.
+        If the system is 0-dimensional (finite), this parameter is ignored.
+
+    Returns
+    -------
+    :
+        1D real array that is the difference between the computed and inputted
+        density matrix parametrisations reduced to the hoppings present in h_int.
+    """
+    rho_reduced = tb_to_qsymm.construct_ham(rho_params, Q_basis)
+    rho_new = model.density_matrix(rho_reduced, nk=nk)
+
+    rho_reduced_new = {key: rho_new[key] for key in model.h_int}
+    rho_params_new = tb_to_qsymm.calculate_coefficients(rho_reduced_new, Q_basis)
+
+    # Calculate the difference between the new params.
+    # (0,): [a1, a2, a3]
+    # Instead of dict, maybe do keys + values
+
+    return rho_params_new - rho_params
+
+
 def solver_mf(
     model: Model,
     mf_guess: np.ndarray,
@@ -101,6 +140,7 @@ def solver_mf(
     """
     shape = model._ndof
     mf_params = tb_to_rparams(mf_guess)
+
     f = partial(cost_mf, model=model, nk=nk)
     result = rparams_to_tb(
         optimizer(f, mf_params, **optimizer_kwargs), list(model.h_int), shape
@@ -150,6 +190,80 @@ def solver_density(
     rho_result = rparams_to_tb(
         optimizer(f, rho_params, **optimizer_kwargs), list(model.h_int), shape
     )
+    mf_result = meanfield(rho_result, model.h_int)
+    fermi = fermi_energy(add_tb(model.h_0, mf_result), model.filling, nk=nk)
+    return add_tb(mf_result, {model._local_key: -fermi * np.eye(model._ndof)})
+
+
+def solver_density_symmetric(
+    model: Model,
+    guess: tuple = None,
+    symmetries: tuple = None,  # We may want to make the symmetries an optional part of the model.
+    nk: int = 20,
+    optimizer: Optional[Callable] = scipy.optimize.anderson,
+    optimizer_kwargs: Optional[dict[str, str]] = {"M": 0, "line_search": "wolfe"},
+) -> _tb_type:
+    """Solve for the mean-field correction through self-consistent root finding
+    by finding the density matrix fixed point.
+
+    Parameters
+    ----------
+    model :
+        Interacting tight-binding problem definition.
+    mf_guess :
+        The initial guess for the mean-field correction in the tight-binding dictionary format.
+    nk :
+        Number of k-points in a grid to sample the Brillouin zone along each dimension.
+        If the system is 0-dimensional (finite), this parameter is ignored.
+    optimizer :
+        The solver used to solve the fixed point iteration.
+        Default uses `scipy.optimize.anderson`.
+    optimizer_kwargs :
+        The keyword arguments to pass to the optimizer.
+
+    Returns
+    -------
+    :
+        Mean-field correction solution in the tight-binding dictionary format.
+    """
+
+    shape = model._ndof
+
+    # User should provide a guess that is a tuple of (bloch_family, coefficients).
+    # If this is not provided, we generate these ourselves.
+    if guess == None:
+        ham_fam = tb_to_qsymm.tb_to_ham_fam((model.h_0, model.h_int), symmetries)
+        Q_basis = tb_to_qsymm.QR_decomposition(ham_fam)
+
+        scale = 5  # Arbitrary right now
+        random_coeffs = {}
+        # Can these be complex?
+        for hopping in Q_basis:
+            amplitude = scale * np.random.rand(len(Q_basis[hopping]))
+            phase = 2 * np.pi * np.random.rand(len(Q_basis[hopping]))
+            random_coeffs[hopping] = amplitude * np.exp(1j * phase)
+
+        mf_guess = tb_to_qsymm.construct_ham(random_coeffs, Q_basis)
+    else:
+        ham_fam, coefficients = guess
+        Q_basis = tb_to_qsymm.QR_decomposition(ham_fam)
+
+        mf_guess = tb_to_qsymm.construct_ham(coefficients, Q_basis)
+
+    # Not the right density matrix function.
+    rho_guess = density_matrix(
+        add_tb(model.h_0, mf_guess), filling=model.filling, nk=nk, kT=model.kT
+    )[0]
+    rho_guess_reduced = {key: rho_guess[key] for key in model.h_int}
+
+    rho_params = tb_to_qsymm.calculate_coefficients(rho_guess_reduced, Q_basis)
+
+    f = partial(cost_density_symmetric, model=model, Q_basis=Q_basis, nk=nk)
+    rho_result = tb_to_qsymm.construct_ham(
+        optimizer(f, rho_params, **optimizer_kwargs), Q_basis
+    )
+
+    # Not sure after this yet
     mf_result = meanfield(rho_result, model.h_int)
     fermi = fermi_energy(add_tb(model.h_0, mf_result), model.filling, nk=nk)
     return add_tb(mf_result, {model._local_key: -fermi * np.eye(model._ndof)})
