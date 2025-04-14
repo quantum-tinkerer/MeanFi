47389c789795e7a0b390e2164c0bbcc57f086368
kyudomaster <jasper.brookman@kpnmail.nl>
2025-04-14 17:18:14 +0200

    Added a flattening and unflattening test for the coefficients.

--- meanfi/tests/test_params.py
+++ meanfi/tests/test_params.py
@@ -1,17 +1,20 @@
 # %%
 import pytest
 import numpy as np
-from meanfi.params.rparams import rparams_to_tb, tb_to_rparams
+from meanfi.params.rparams import (
+    rparams_to_tb,
+    tb_to_rparams,
+    flatten_qparams,
+    unflatten_qparams,
+)
 from meanfi.tb.tb import compare_dicts
-from meanfi.tb.utils import generate_tb_keys
+from meanfi.tb.utils import generate_tb_keys, guess_coeffs
 from meanfi import guess_tb
 
 repeat_number = 10
+
+
 # %%
-ndof = 10
-vectors = ((0, 0), (1, 0), (-1, 0), (0, 1), (0, -1), (1, -1), (-1, 1), (1, 1), (-1, -1))
-
-
 @pytest.mark.parametrize("seed", range(repeat_number))
 def test_parametrisation(seed):
     """Test the parametrisation of the tight-binding model."""
@@ -26,3 +29,13 @@ def test_parametrisation(seed):
         mf_params = tb_to_rparams(mf_guess)
         mf_new = rparams_to_tb(mf_params, list(mf_guess), ndof)
         compare_dicts(mf_guess, mf_new)
+
+        basis_guess = {}
+        for key in keys:
+            basis_guess[key] = np.zeros((np.random.randint(1, 10), ndof, ndof))
+
+        coeff_guess = guess_coeffs(basis_guess)
+
+        flat_coeffs = flatten_qparams(coeff_guess)
+        new_coeffs = unflatten_qparams(flat_coeffs, basis_guess)
+        compare_dicts(coeff_guess, new_coeffs)
