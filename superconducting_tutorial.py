4ead29f0f285a4098dd66aeb137c3b17825d932b
kyudomaster <jasper.brookman@kpnmail.nl>
2025-05-19 08:41:58 +0200

    `superconducting_tutorial.py` is the example for Jasper's thesis.

--- superconducting_tutorial.py
+++ superconducting_tutorial.py
@@ -1,4 +1,5 @@
-# %%
+# %% Example
+# We import all the required functions.
 from meanfi.tb.utils import superc_tb
 import qsymm
 from meanfi.tb.transforms import tb_to_ham_fam
@@ -7,35 +8,44 @@ from meanfi.model import Model
 from meanfi.tb.utils import symm_guess_mf
 from meanfi.solvers import solver_density_symmetric
 
+tau_x = np.array([[0, 1], [1, 0]])
+tau_z = np.array([[1, 0], [0, -1]])
+
+# %% We build a Hamiltonian for our system.
 cutoff = 1
 ndim = 1
 ndof = 1
 
 h_0 = superc_tb(cutoff, ndim, ndof)
 h_int = superc_tb(cutoff, ndim, ndof)
-tau_z = np.array([[1, 0], [0, -1]])
-Q = np.kron(tau_z, np.eye(ndof))
-target_Q = 0
-kT = 1e-2
-tau_x = np.kron(np.array([[0, 1], [1, 0]]), np.eye(ndof))
+# h_0 =
+# h_int =
 
-PHS = qsymm.particle_hole(ndim, tau_x)
+# %% We set up the `meanfi` model.
+charge_op = np.kron(tau_z, np.eye(ndof))
+target_charge = 0
+kT = 0
 
+model = Model(h_0, h_int, target_charge, charge_op, kT)
+
+# %% We build the bloch_family.
+# Particle-hole symmetry
+PHS = qsymm.particle_hole(ndim, np.kron(tau_x, np.eye(ndof)))
 symmetries = [PHS]
 
-model = Model(h_0, h_int, Q, target_Q, kT)
+# We recalculate the ndof, since we now have double it for the holes and electrons.
+ndof = list(h_0.values())[0].shape[-1]
+
 hams = (h_0, h_int)
+hoppings = list(h_int.keys())
 
-ndof = 2  # This is different because we generated the superconducting tb with ndof 1 per particle. (but there are two particles)
-hoppings = list(
-    h_int.keys()
-)  # I believe this should still contain all hoppings, including h_0
 ham_fam = tb_to_ham_fam(hoppings, ndof, symmetries)
-mf_guess = symm_guess_mf(
-    ham_fam
-)  # This function causes it to orthogonalize twice now, but since there are other ways of creating the guess, I think it's fine.
 
-print(h_0)
-print(h_int)
+# %% We generate an initial guess with the ham_fam.
+mf_guess = symm_guess_mf(ham_fam)
 
-print(solver_density_symmetric(model, ham_fam, mf_guess, nk=50))
+# %% We compute the self-consistent solution and observables.
+ham_solution = solver_density_symmetric(model, ham_fam, mf_guess, nk=100)
+print(ham_solution)
+
+observable_1 = ...
