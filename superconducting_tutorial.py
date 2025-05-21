dc5933c87a69c88e774c0df21ecb205dfd1b5b84
kyudomaster <jasper.brookman@kpnmail.nl>
2025-05-21 18:14:14 +0200

    Updated `h_int` in hte superconducting tutorial. We have a superconducting result :)

--- superconducting_tutorial.py
+++ superconducting_tutorial.py
@@ -6,16 +6,18 @@ import numpy as np
 from meanfi.model import Model
 from meanfi.tb.utils import symm_guess_mf
 from meanfi.solvers import solver_density_symmetric
+import matplotlib.pyplot as plt
 
 tau_x = np.array([[0, 1], [1, 0]])
 tau_z = np.array([[1, 0], [0, -1]])
+tau_0 = np.eye(2)
 
 # %% We build a Hamiltonian for our system.
 # We build it using blocks of h_0 and Delta, or h_int and Delta.
 ndof = 2
 ndim = 1
 
-delta = 0
+delta = 0.1
 
 h_sc_0 = {
     (0,): delta * np.kron(tau_x, np.eye(ndof)),
@@ -23,8 +25,10 @@ h_sc_0 = {
     (-1,): np.kron(tau_z, np.eye(ndof)),
 }
 
+U = -2
 h_sc_int = {
-    (0,): delta * np.kron(tau_x, np.eye(ndof)) + np.kron(np.eye(2), np.eye(ndof))
+    (0,): U
+    * (np.kron(tau_0, np.ones((ndof, ndof))) - np.kron(tau_x, np.ones((ndof, ndof))))
 }
 
 # We recalculate the ndof, since we now have double it for the holes and electrons.
@@ -33,7 +37,7 @@ ndof_sc = ndof * 2
 # %% We set up the `meanfi` model.
 charge_op = np.kron(tau_z, np.eye(ndof))
 target_charge = 0
-kT = 0
+kT = 1e-3
 
 model = Model(h_sc_0, h_sc_int, target_charge, charge_op, kT)
 
@@ -52,8 +56,13 @@ ham_fam = tb_to_ham_fam(hoppings, ndof_sc, symmetries)
 mf_guess = symm_guess_mf(ham_fam)
 
 # %% We compute the self-consistent solution and observables.
-ham_solution = solver_density_symmetric(model, ham_fam, mf_guess, nk=100)
-print(ham_solution)
+ham_solution = solver_density_symmetric(
+    model, ham_fam, mf_guess, nk=100, optimizer_kwargs={"verbose": True}
+)
+
+plt.title(f"U = {U}")
+plt.imshow(np.abs(ham_solution[(0,)]), cmap="coolwarm")
+plt.colorbar()
 
 observable_1 = ...
 
